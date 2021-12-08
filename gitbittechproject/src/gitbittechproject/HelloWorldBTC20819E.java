31e58a92a2b2a7f9778914b6a16e04074e57390a
meghamapala@yahoo.com <meghamapala@yahoo.com>
2021-12-08 15:29:38 -0500

    second commit

---
+++ gitbittechproject/src/gitbittechproject/HelloWorldBTC20819E.java
@@ -0,0 +1,14 @@
+package gitbittechproject;
+
+public class HelloWorldBTC20819E {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i =0; i<=10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
