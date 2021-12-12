fe7528e220d5341d8899afb0aa17126d6a17ad2e
meghamapala@yahoo.com <meghamapala@yahoo.com>
2021-12-12 15:32:48 -0500

    add to project

---
+++ strinmanupulation07/src/strinmanupulation07/String_Man_IndexOf.java
@@ -0,0 +1,15 @@
+package strinmanupulation07;
+
+public class String_Man_IndexOf {
+
+	public static void main(String[] args) {
+		
+		String ff = "I love my work please come to me";
+
+		int bb=ff.indexOf("z");
+		System.out.println(bb);
+
+
+	}
+
+}
