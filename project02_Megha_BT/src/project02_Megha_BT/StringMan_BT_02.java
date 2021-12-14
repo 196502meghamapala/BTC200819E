00a00e43e536b2ee9fe2d1547a420d5f5d8e994d
meghamapala@yahoo.com <meghamapala@yahoo.com>
2021-12-14 13:46:32 -0500

    first commit

---
+++ project02_Megha_BT/src/project02_Megha_BT/StringMan_BT_02.java
@@ -0,0 +1,15 @@
+package project02_Megha_BT;
+
+public class StringMan_BT_02 {
+
+	public static void main(String[] args) {
+		
+		String kk = "Java has four concept of OOP";
+		String s1[]=kk.split(" ");
+		for (int i=0; i<s1.length;  i++) {
+			System.out.println(s1[i]);
+		}
+
+	}
+
+}
