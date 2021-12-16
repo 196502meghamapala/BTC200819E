8732f24784877b35e99e698f0949fa0e2cc9007d
meghamapala@yahoo.com <meghamapala@yahoo.com>
2021-12-16 09:42:14 -0500

    array list

---
+++ listproject/src/listproject/ListBT.java
@@ -0,0 +1,26 @@
+package listproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListBT {
+
+	public static void main(String[] args) {
+		
+		int a[]= {7,9,10,3,15,20};
+		
+		List<Integer> list2 = new ArrayList<Integer>();
+		for(int number:a) {
+	         list2.add(number);
+	      }
+	      System.out.println(list2);
+	      
+	      
+	      
+	      
+	      
+		
+
+	}
+
+}
--- stringmanupulation08/src/stringmanupulation08/Scanner_sc.java
+++ stringmanupulation08/src/stringmanupulation08/Scanner_sc.java
@@ -1,9 +1,12 @@
 package stringmanupulation08;
 
+import java.util.Scanner;
+
 public class Scanner_sc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
 
 	}
 
